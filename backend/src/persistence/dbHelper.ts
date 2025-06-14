1e6c5d1b6f180b26840dc6cb807cf0777fe23e40
Ace Beattie <acebeattie@gmail.com>
2025-06-14 10:22:36 -0400

    use vite and pnpm

--- backend/src/persistence/dbHelper.ts
+++
@@ -1,6 +0,0 @@
-import {Sequelize} from 'sequelize';
-export const sequelize = new Sequelize({
-    dialect: 'sqlite',
-    storage: process.env.DATABASE_PATH ?? "./undefinedDatabase.sqlite",
-    logging: (process.env.DATABASE_LOGGING === "true")
-});
